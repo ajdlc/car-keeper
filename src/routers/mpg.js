c287b7ae5e552caba8d7b63c2e481ecb02219754
AJ De La Costa <ajdlc47@gmail.com>
2020-02-05 14:47:55 -0800

    Almost done with MPG entry router

--- src/routers/mpg.js
+++ src/routers/mpg.js
@@ -39,4 +39,90 @@ router.post("/cars/:id/mpg", auth, async (req, res) => {
     }
 });
 
+// READ mpg entry
+router.get("/cars/:id/mpg/:mpgId", auth, async (req, res) => {
+    try {
+        const car = await Car.findOne({ _id: req.params.id, owner: req.user._id });
+
+        if (!car) {
+            return res.status(404).send();
+        }
+
+        const mpgEntry = car.mpg.find(entry => entry.id === req.params.mpgId);      
+
+        if (mpgEntry.length === 0) {
+            return res.status(404).send();
+        }
+
+        res.send(mpgEntry)
+    } catch (e) {
+        res.send(500).send();
+    }
+
+});
+
+// READ MPG Entries
+router.get("/cars/:id/mpg", auth, async (req, res) => {
+    try {
+        const car = await Car.findOne({ _id: req.params.id, owner: req.user._id });
+
+        if (!car) {
+            return res.status(404).send();
+        }
+
+        res.send(car.mpg);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+// UPDATE MPG entry
+router.patch("/cars/:id/mpg/:mpgId", auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["miles", "gallons", "mpg", "location", "date"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: "Invalid operation"});
+    }
+
+    try {
+        const car = await Car.findOne({ _id: req.params.id, owner: req.user._id });
+
+        if (!car) {
+            return res.status(404).send();
+        }
+
+        // Get the index of the mpg entry we need
+        const entryIndex = car.mpg.findIndex(entry => entry.id === req.params.mpgId);
+
+        if (entryIndex === -1) {           
+            return res.status(404).send();
+        }
+
+        updates.forEach(update => {
+            car.mpg[entryIndex][update] = req.body[update];
+        })
+
+        // Determine if the MPG needs to be recalculated
+        if ((updates.includes("miles") || updates.includes("gallons")) && !updates.includes("mpg")) {
+            // Recalculate mpg
+            car.mpg[entryIndex].mpg = (car.mpg[entryIndex].miles / car.mpg[entryIndex].gallons).toFixed(2);
+        }
+
+        // Determine if the date needs to be changed
+        if (!updates.includes("date")) {
+            car.mpg[entryIndex].date = car.mpg[entryIndex].date;
+        } else {
+            // Convert date
+            car.mpg[entryIndex].date = new Date(req.body.date);
+        }
+        
+        await car.save();
+        res.send(car);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
+
 module.exports = router;
