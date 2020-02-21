772526f0355558b496ecc343231e988c90bb6b12
ajdlc <ajdlc47@gmail.com>
2020-02-21 09:39:40 -0800

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -17,16 +17,16 @@ app.use(express.json());
 // app.use(session({secret: process.env.SESSION_KEY}));
 
 // For CORS - Only when being used with development
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     if ("OPTIONS" == req.method) {
-//         res.send();
-//     } else {
-//         next();
-//     }
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if ("OPTIONS" == req.method) {
+        res.send();
+    } else {
+        next();
+    }
+});
 
 
 // Use the user router
